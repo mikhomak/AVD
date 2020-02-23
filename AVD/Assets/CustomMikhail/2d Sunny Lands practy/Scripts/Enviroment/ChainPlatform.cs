180ea5bd781902de6c191dcb5c2741e0b469d0ad
Misha <homakhaker@gmail.com>
2020-02-23 17:03:32 +0100

    Added more stuff

---
+++ AVD/Assets/CustomMikhail/2d Sunny Lands practy/Scripts/Enviroment/ChainPlatform.cs
@@ -0,0 +1,18 @@
+﻿using CustomMikhail._2d_Sunny_Lands_practy.Scripts.Player;
+using CustomMikhail._2d_Sunny_Lands_practy.Scripts.Player.Movement;
+using UnityEngine;
+
+namespace CustomMikhail._2d_Sunny_Lands_practy.Scripts.Enviroment {
+    public class ChainPlatform : MonoBehaviour {
+        [SerializeField] private HingeJoint2D hingeJoint2D;
+
+
+        private void OnTriggerEnter2D(Collider2D other) {
+            var player = other.GetComponent<IPlayer>();
+            if (player?.getMovement() is EagleMovement) {
+                hingeJoint2D.enabled = false;
+                Destroy(gameObject);
+            }
+        }
+    }
+}
---
+++ AVD/Assets/CustomMikhail/2d Sunny Lands practy/Scripts/Enviroment/EnableGameObjectAndDestroy.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnableGameObjectAndDestroy : MonoBehaviour {
+    [SerializeField] private GameObject gameObjectToEnable;
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        if (other.tag.Equals("Player")) {
+            gameObjectToEnable.SetActive(true);
+            Destroy(this);
+        }
+    }
+}
