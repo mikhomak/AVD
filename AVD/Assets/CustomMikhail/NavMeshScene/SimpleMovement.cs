8fd5671a18d31c973667017e711bf3f6c8073d5f
Misha <homakhaker@gmail.com>
2020-03-20 19:49:25 +0100

    NavMeshPract

---
+++ AVD/Assets/CustomMikhail/NavMeshScene/SimpleMovement.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.PlayerLoop;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class SimpleMovement : MonoBehaviour {
+    private Animator animator;
+    private NavMeshAgent agent;
+    RaycastHit hitInfo = new RaycastHit();
+
+    void Start() {
+        agent = GetComponent<NavMeshAgent>();
+        animator = GetComponentInChildren<Animator>();
+    }
+
+    private void FixedUpdate() {
+        if (Input.GetMouseButton(0)) {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray.origin, ray.direction, out hitInfo)) {
+                agent.destination = hitInfo.point;
+            }
+        }
+    }
+
+}
